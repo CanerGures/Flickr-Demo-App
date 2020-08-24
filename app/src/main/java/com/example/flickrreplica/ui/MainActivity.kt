eb5469d959748332b114922840944c96778775a0
YusufCanerGures <gurescaner@gmail.com>
2020-08-24 10:09:25 +0300

    Initial. Coded with love.

---
+++ app/src/main/java/com/example/flickrreplica/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flickrreplica.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flickrreplica.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
