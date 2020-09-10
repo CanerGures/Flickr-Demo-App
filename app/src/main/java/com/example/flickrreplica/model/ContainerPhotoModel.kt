7bb3337fef48ad9895bdcc6438610fc1a033fe0b
YusufCanerGures <gurescaner@gmail.com>
2020-09-10 12:20:53 +0300

    Refactoring.

--- app/src/main/java/com/example/flickrreplica/model/ContainerPhotoModel.kt
+++ app/src/main/java/com/example/flickrreplica/model/ContainerPhotoModel.kt
@@ -2,13 +2,6 @@ package com.example.flickrreplica.model
 
 import java.io.Serializable
 
-data class UrlBuilderModel(
-    val farmId: String,
-    val serverId: String,
-    val secretId: String,
-    val id: String
-)
-
 data class ContainerPhoto(
     val id: String,
     val url: String,
